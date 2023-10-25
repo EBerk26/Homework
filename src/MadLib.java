8a94fff9a375a44577277d2be0ca8ae4f1b5c655
EBerk26 <eli_berk26@milton.edu>
2023-10-25 10:28:42 -0400

    HW due Tuesday 10.31.2023

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,11 +1,38 @@
-public class MadLib {
-    public MadLib(int line){
-        //this class is a constructor - it creates objects
-        System.out.println("line " + line);
-    }
+public class MadLib{
     public static void main(String[] args) {
-        for(int x = 1; x<=1000000; x++){
-            new MadLib(x);
-        }
+        MadLib myStory = new MadLib();
+    }
+    public MadLib(){
+        String classmate = "Teddy";
+        String city = "Boston";
+        String noun = "egg";
+        String adjective = "smelly";
+        String pluralNoun = "bowling balls";
+        String pluralAnimal = "chickens";
+        boolean trueOrFalse = true;
+        double decimalBiggerThan1 = 1.4;
+        int number1 = 8;
+        int number2 = 19;
+        int wholeNumberBetween1And4 = 2;
+        String miltonTeacher = "Mx. Bradford";
+        String miltonDean = "Mr. Ruiz";
+        char letterGrade = 'B';
+        String season = "fall";
+        //define a string called story for mad lib
+        String story = "This weekend I am going camping with "
+                +classmate+ ". We are going to a campsite in "
+                +city+ ". I packed my "+noun+ " and "
+                +pluralNoun + ".  The forecast is calling for a high of "
+                +decimalBiggerThan1+ " degrees fahrenheit. So it should be a "
+                +adjective+" day! This year, the park rangers have seen "
+                +number1+ " " +pluralAnimal + " which seems kind of dangerous. "
+                +miltonTeacher+" said it's " + trueOrFalse + " that "+pluralAnimal+ " eat class "
+                +wholeNumberBetween1And4+ " students in the " +season+ ". But, "
+                +miltonDean+ " said it actually has only happened "
+                + number2+" times. Wish us luck! If we survive, we will earn a "
+                +letterGrade+ "- in P.E. class.";
+//now print story to the dos window
+        System.out.println(story);
+
     }
 }
